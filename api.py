ba4a4b5271ab55dbd20c9ebac6fdd06c03c33b83
unknown <egrsup769@gmail.com>
2025-10-04 19:13:35 +0300

    added viewing one employee at a time and changing manager

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from database import Employee, db
 from functions import sorting
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/employees'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
 
@@ -12,7 +12,7 @@ API_ROUTE = '/api/v1'
 EMPLOYEES_API_ROUTE = '/employees'
 
 
-@app.route(EMPLOYEES_API_ROUTE, methods=['GET'])
+@app.route(EMPLOYEES_API_ROUTE+'/', methods=['GET'])
 def read_all():
     query = Employee.query
     
@@ -25,8 +25,20 @@ def read_all():
         max_per_page=100,
         error_out=False,
     )
-    return render_template("all_employees.html", employees=employees, title='all employees')
+    return render_template("all_employees.html", employees=employees, title='all employees'), 200
 
+@app.route(EMPLOYEES_API_ROUTE+'/<int:id>/', methods=['GET'])
+def read_one(id):
+    employee = db.get_or_404(Employee, id)
+    return render_template("one_employee.html", employee=employee, title=f'employee id:{id}'), 200
+
+
+@app.route(API_ROUTE+EMPLOYEES_API_ROUTE+'/<int:id>/edit', methods=['POST'])
+def change_manager(id):
+    employee = db.get_or_404(Employee, id)
+    employee.manager_id = int(request.form['manager'])
+    db.session.commit()
+    return redirect(url_for("read_one", id=employee.id))
 
 with app.app_context():
     db.create_all()
